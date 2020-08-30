7baffc400d76b73ec7a21b0bd56bf0629221610d
Michal "mišo" Červeňák <miso@fykos.cz>
2020-08-30 12:35:15 +0200

    Fix php 7.4 compoatibility

--- conf/metadata.php
+++ conf/metadata.php
@@ -5,10 +5,10 @@
  *
  * @author Michal Koutný <michal@fykos.cz>
  */
-$meta['mysql_host'] = array('string');
-$meta['mysql_user'] = array('string');
-$meta['mysql_password'] = array('password');
-$meta['mysql_database'] = array('string');
-$meta['contest'] = array('multichoice', '_choices' => array('fykos', 'vyfuk'));
-$meta['fallback_enabled'] = array('onoff');
-$meta['fallback_plugin'] = array('string');
+$meta['mysql_host'] = ['string'];
+$meta['mysql_user'] = ['string'];
+$meta['mysql_password'] = ['password'];
+$meta['mysql_database'] = ['string'];
+$meta['contest'] = ['multichoice', '_choices' => ['fykos', 'vyfuk']];
+$meta['fallback_enabled'] = ['onoff'];
+$meta['fallback_plugin'] = ['string'];
